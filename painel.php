6ab08979409cc156744a444a0ab981e21c0f8bd1
micaeleee <49121243+micaeleee@users.noreply.github.com>
2019-10-05 15:23:43 -0300

    Update painel.php

--- painel.php
+++ painel.php
@@ -3,8 +3,8 @@
    $user = "root";
    $pass = "";
    $banco = "cadastro";
-   $conexao = mysql_connect($host, $user, $pass) or die(mysql_error());
-   mysql_select_db($banco) or die(mysql_error());
+   $conexao = @mysql_connect($host, $user, $pass) or die(@mysql_error());
+   @mysql_select_db($banco) or die(@mysql_error());
 ?>
 <?php
     session_start();
