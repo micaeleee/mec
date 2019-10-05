d2894198b965b998e8a9e5225d7b023325645c71
micaeleee <49121243+micaeleee@users.noreply.github.com>
2019-10-05 14:57:02 -0300

    Update cadastrando.php

--- cadastrando.php
+++ cadastrando.php
@@ -12,7 +12,8 @@
    $user = "root";
    $pass = "";
    $banco = "cadastro";
-   $conexao = mysql_connect($host, $user, $pass) or die(mysql_error());
+   $conexao = @mysql_connect($host, $user, $pass) or die(mysql_error());
+    $con = @mysql_connect($host, $user, $pass) or die(mysql_error());
    mysql_select_db($banco) or die(mysql_error());
  ?>
 
